5c4918341824274fad10820e4b62d085aa0db1bf
blackamlet <paskalzlo@bk.ru>
2017-12-16 04:18:12 +0300

    форма добавления книг css

---
+++ main.js
@@ -0,0 +1,15 @@
+/* (function () {
+    // в этой задаче неважно, как именно прятать элемент
+        // например через style.display:
+
+        var a = document.getElementsByClassName('btn');
+        var b = document.getElementsByClassName('addition-form');
+        a.onclick = function() {
+          b.style.display = 'flex';
+        }
+}()) */
+
+(function () {
+    var b = document.getElementsByClassName("filter-results");
+    b.style.background = red;
+})
