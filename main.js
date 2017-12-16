aa19c7c3ef5d697b9d840ec59e681b0ce1daa5ea
blackamlet <paskalzlo@bk.ru>
2017-12-16 17:09:05 +0300

    Чуть пофиксил библиотеку, оживил кнопку добавления книг

--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-/* (function () {
+
+
+(function () { /* Всплывающее окошко при на нажатии на 'Add a Book' */
     // в этой задаче неважно, как именно прятать элемент
         // например через style.display:
 
-        var a = document.getElementsByClassName('btn');
-        var b = document.getElementsByClassName('addition-form');
-        a.onclick = function() {
-          b.style.display = 'flex';
-        }
-}()) */
+    var a = document.getElementById('add-book');
+    var b = document.getElementById('add-form');
+    a.onclick = function() {
+        b.style.display = 'flex';
+    };
 
-(function () {
-    var b = document.getElementsByClassName("filter-results");
-    b.style.background = red;
-})
+
+}());
+
+
+(function () { /* добавление книг */
+    var createBook = document.getElementsByClassName('form-create-book');
+    var addAuthorName = document.getElementsByClassName('form-author-name');
+    var addBookName = document.getElementsByClassName('form-book-name');
+    var filterResults = document.getElementsByClassName('filter-results');
+    var b = document.getElementById('add-form');
+
+    createBook[0].onclick = function(){
+
+        var bookCover = document.createElement('div');
+        bookCover.className = 'book-cover';
+
+
+        var bookName = document.createElement('div');
+        bookName.className = 'book-name';
+        bookName.innerHTML = addBookName[0].value;
+
+        var bookAuthorName = document.createElement('div');
+        bookAuthorName.className = 'book-author';
+        bookAuthorName.innerHTML = addAuthorName[0].value;
+
+        
+        var book = document.createElement('div');
+        book.className = 'book-content';
+        book.appendChild(bookCover);
+        book.appendChild(bookName);
+        book.appendChild(bookAuthorName);
+
+        filterResults[0].appendChild(book);
+        
+        b.style.display = 'none';
+    }  
+
+    
+}())
+
+
